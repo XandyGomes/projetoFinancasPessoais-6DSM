e58e2ec1cfefd5485da9398501bdf3b3f9970741
XandyGomes <x3.computadores@hotmail.com>
2024-03-28 15:35:02 -0300

    Aula.28.03.2024

--- backend/src/main/java/com/gomes/financaspessoais/service/LancamentoService.java
+++ backend/src/main/java/com/gomes/financaspessoais/service/LancamentoService.java
@@ -1,5 +1,22 @@
 package com.gomes.financaspessoais.service;
 
+import java.util.List;
+
+import com.gomes.financaspessoais.model.entity.Lancamento;
+import com.gomes.financaspessoais.model.enums.StatusLancamento;
+
 public interface LancamentoService {
 
+	Lancamento salvar(Lancamento lancamento);
+	
+	Lancamento atualizar(Lancamento lancamento);
+	
+	void deletar(Lancamento lancamento);
+	
+	List<Lancamento> buscar( Lancamento lancamentoFiltro);
+	
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+	
+	void validar(Lancamento lancamento);
+	
 }
